f10fb85d39cfe668f790ba5c9da01c9852150889
caoquy2000 <caohoanghuy2000@gmail.com>
2020-12-18 17:36:12 +0700

    update second

--- js/update.js
+++ js/update.js
@@ -1,10 +1,25 @@
-$('#btn-baogia').click(function(){
+$('#btn-baogia').click(function () {
     $('html, body').animate({
         scrollTop: $('#baogia').offset().top
-    },2000);
+    }, 2000);
 });
-$('#btn-dichvu').click(function(){
+$('#btn-dichvu').click(function () {
     $('html, body').animate({
         scrollTop: $('#dichvu').offset().top
-    },2000);
+    }, 2000);
+});
+var typed2 = new Typed('#typed2', {
+    strings: ['<h2>Vì Sao Nên Chọn Bảo Vệ Thanh Long</h2>'],
+    typeSpeed: 20,
+    backSpeed: 20,
+    fadeOut: true,
+    loop: true
+});
+
+var typed3 = new Typed('#typed3', {
+    strings: ['<p style="font-size:20px">Với đội ngũ lãnh đạo dày dặn kinh nghiệm, chúng tôi tự tin là đơn vị cung cấp dịch vụ bảo vệ uy tín chất lượng cao được nhiều đối tác và khách hàng tin tưởng trong nhiều năm.</p>'],
+    typeSpeed: 10,
+    backSpeed: 10,
+    fadeOut: true,
+    loop: true
 });
