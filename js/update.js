114c6e4c94d946199e9899d378bd5d0460ea18e0
caoquy2000 <caohoanghuy2000@gmail.com>
2020-12-18 12:13:12 +0700

    update

---
+++ js/update.js
@@ -0,0 +1,10 @@
+$('#btn-baogia').click(function(){
+    $('html, body').animate({
+        scrollTop: $('#baogia').offset().top
+    },2000);
+});
+$('#btn-dichvu').click(function(){
+    $('html, body').animate({
+        scrollTop: $('#dichvu').offset().top
+    },2000);
+});
